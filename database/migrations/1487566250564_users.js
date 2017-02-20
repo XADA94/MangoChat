7e21920fd7c8b31ae187cf501e09943abce1ac09
Daniel Torres <danieljtorres94@gmail.com>
2017-02-20 01:03:28 -0400

    Migracion para tabla usuarios

---
+++ database/migrations/1487566250564_users.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class UsersTableSchema extends Schema {
+
+  up () {
+    this.create('users', (table) => {
+      table.increments()
+      table.string('nickname').unique()
+      table.string('avatar').unique().defaultTo("avatar.png")
+      table.string('email').unique()
+      table.string('password', 60)
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('users')
+  }
+
+}
+
+module.exports = UsersTableSchema
